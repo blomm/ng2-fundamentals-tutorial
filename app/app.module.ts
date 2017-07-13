013d97aab4acffc3e07374271159e6840fc4f04f
michael blom <mike_geomatics@yahoo.com>
2017-07-13 07:42:49 +0100

    added ngclass samples

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,11 +5,14 @@ import {EventsAppComponent} from './events-app.component'
 import {EventsListComponent} from './events/events-list.component'
 import {EventThumbnailComponent} from './events/event-thumbnail.component'
 import {NavBarComponent} from './nav/nav.component'
+import { EventService } from "./events/shared/event.service";
+import { HttpModule } from "@angular/http";
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule,HttpModule],
     declarations: [EventsAppComponent,EventsListComponent, EventThumbnailComponent, NavBarComponent],
-    bootstrap: [EventsAppComponent]
+    bootstrap: [EventsAppComponent],
+    providers:[EventService]
 })
 
 export class AppModule {}
--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,7 +4,9 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
     selector:'event-thumbnail',
     templateUrl:'app/events/event-thumbnail.html',
     styles:[`
-    .thumbnail {min-height: 210px}
+        .thumbnail {min-height: 210px}
+        .green {color: #003300 !important}
+        .bold {font-weight: bold}
         .pad-left {margin-left: 20px;}
         .well div {color: #bbb;}
     `]
@@ -13,4 +15,8 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
 export class EventThumbnailComponent{
     @Input() event:any
     
+    getStartTimeClass(){
+        const isEarly = this.event && this.event.time == '8:00 am';
+        return  {green: isEarly, bold: isEarly}
+    }
 }
